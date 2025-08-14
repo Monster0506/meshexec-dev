d5b7ab04b09e8f24576284e91cb697b4daa2a9dc
Monster0506 <tjraklovits@gmail.com>
2025-08-13 23:24:06 -0400

    Remove unsupported tryNewSidecarTransport function for non-Windows builds

--- internal/ble/transport_sidecar_stub.go
+++ internal/ble/transport_sidecar_stub.go
@@ -6,9 +6,3 @@ import (
 	core "github.com/monster0506/meshexec/internal"
 	"github.com/monster0506/meshexec/internal/logging"
 )
-
-// tryNewSidecarTransport is unavailable on non-Windows builds.
-// Return (nil, false, nil) to signal not supported so factory can skip it.
-func tryNewSidecarTransport(cfg *core.NetworkConfig, logger *logging.Logger) (core.BLETransport, bool, error) {
-	return nil, false, nil
-}
